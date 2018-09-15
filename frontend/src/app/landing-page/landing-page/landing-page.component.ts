94f450bfc0707618ca2b036678f1b7f211d8fd7b
Andriy Danylko <andriy.danylko@gmail.com>
2018-09-15 19:34:22 +0200

    create select

--- frontend/src/app/landing-page/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page/landing-page.component.ts
@@ -7,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
+  selectForm = 'form1';
+
   constructor() { }
 
   ngOnInit() {
 
+  }
+
+  selectChangeHandler(event: any) {
+    this.selectForm = event.target.value;
   }
 
 
